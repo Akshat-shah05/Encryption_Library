382874085cb01418eefc7ae17dd42271f686dc7d
akshat-shah05 <akshatshah1605@gmail.com>
2024-07-14 18:35:32 -0400

    var change for decrypt + export __attribute__

--- arcfour/arcfour.h
+++ arcfour/arcfour.h
@@ -6,8 +6,10 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <errno.h>
-#define rc4decrypt(x, y)   rc4encrypt(x, y)
-#define rc4destroy(x)      free(x)
+
+#define export              __attribute__((visibility("default"))) // for exporting the functions to the end users who use the encrypt and init functions
+#define rc4decrypt(x, y, z) rc4encrypt(x, y, z)
+#define rc4destroy(x)       free(x)
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
@@ -22,6 +24,6 @@ struct s_arcfour {
 
 typedef struct s_arcfour Arcfour;
 
-Arcfour *rc4init(int8*, int16);
-int8 rc4byte(Arcfour*);
-int8 *rc4encrypt(Arcfour*, int8*, int16);
+export Arcfour *rc4init(int8*, int16);
+int8 rc4byte(Arcfour*); // doesn't need to be exported, not used by end user.
+export int8 *rc4encrypt(Arcfour*, int8*, int16);
